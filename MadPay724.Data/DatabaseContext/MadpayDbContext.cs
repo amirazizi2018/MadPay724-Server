6ed80f1ce874de5a7e4f9ca4b807e5fd70c04426
Amir Azizi <amirazizi67@gmail.com>
2020-10-17 13:21:26 +0330

    ایجاد مدل یوزر

---
+++ MadPay724.Data/DatabaseContext/MadpayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MadPay724.Data.DatabaseContext
+{
+	class MadpayDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=ZIZO-HPC;Initial Catalog=MadPay724db;Integrated Security=true;MultipleActiveResultSets=true;");
+		}
+	}
+}
