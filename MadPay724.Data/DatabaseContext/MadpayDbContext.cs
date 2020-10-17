80dea037e18e0fcfdb951252eb45afdf77c5d865
Amir Azizi <amirazizi67@gmail.com>
2020-10-17 13:54:35 +0330

    ماگریشن دیتا بیس

--- MadPay724.Data/DatabaseContext/MadpayDbContext.cs
+++ MadPay724.Data/DatabaseContext/MadpayDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MadPay724.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MadPay724.Data.DatabaseContext
@@ -9,7 +10,13 @@ namespace MadPay724.Data.DatabaseContext
 	{
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer("Data Source=ZIZO-HPC;Initial Catalog=MadPay724db;Integrated Security=true;MultipleActiveResultSets=true;");
+			optionsBuilder.UseSqlServer("Data Source=ZIZO-HPC;Initial Catalog=MadPay724db;Integrated Security=true;MultipleActiveResultSets=true;User Id=sa;Password=P@ssw0rd;");
 		}
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Photo> Photos { get; set; }
+		public DbSet<BankCard> BankCards { get; set; }
+
+		
 	}
 }
